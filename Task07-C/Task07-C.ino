e3a2ae760eb0e773296451361b240389dc65c921
Pumpkinjam <rnjsl1212@naver.com>
2022-04-18 11:06:09 +0900

    Adding alarm function (2)

--- Task07-C/Task07-C.ino
+++ Task07-C/Task07-C.ino
@@ -19,9 +19,13 @@ int alarmHour, alarmMinute;
 
 void checkHour();
 void checkMinute();
+int ctoi(char);
+void playNote(int, int, int);
 
 void setup() {
     Serial.begin(115200);
+    EEPROM.begin(EEPROM_SIZE);
+    ledcAttachPin(buzPin, ledChannel);
 
     Serial.print("Connecting to ");
     Serial.println(ssid);
@@ -46,7 +50,96 @@ void loop(){
     getLocalTime(&ltime);
 
     if (ltime.tm_hour == alarmHour && ltime.tm_minute == alarmMinute) {
-        
+        int val1 = EEPROM.read(0);
+        int val2 = EEPROM.read(1);
+        playNote(0, -1, 0);
+
+        // saved score found
+        if (val1 == 0xAA && val2 == 0x55) {
+            //Serial.printf("%x %x\n", val1, EEPROM.read(0));
+            Serial.println("Reading from EEPROM...");
+
+            /*
+            Serial.println("saved data is: ");
+
+            for (int idx = 0, z = EEPROM.read(2); idx < z; idx++) {
+            Serial.printf("(%d) - %d\n", idx,  EEPROM.read(idx));
+            }
+            Serial.println();
+            */
+            
+            // save data of note, duration, duty
+            for (int idx = 3, z = EEPROM.read(2); idx+2 < z; ) {
+            delay(10);
+            int noteVal = EEPROM.read(idx++);
+            int durVal = EEPROM.read(idx++);
+            int dutyVal = EEPROM.read(idx++);
+            
+            playNote(noteVal, durVal, dutyVal);
+            }
+            
+        }
+        // score input mode
+        else {
+            Serial.println("No data found. Create new one or play temporarily.");
+
+            // stay until input
+            while (Serial.available() < 1) {
+                delay(10);
+                //Serial.println("Waiting Input...");
+                //delay(1000);
+            }
+
+            // transfer serial input to string
+            int len = 0;
+            while (Serial.available() > 0) {
+                line[len++] = Serial.read();
+            }
+            line[len] = '\0';
+
+
+            // save the value if cmdline starts with '<'
+            if (line[0] == '<') {
+                Serial.println("Saving to EEPROM...");
+                
+                int romIdx = 3;
+                for (int lineIdx = 1; lineIdx + 2 < len - 1; lineIdx+=2) {
+                    int noteVal ,dutyVal = 128, durVal = ctoi(line[lineIdx+1]);
+                    if (line[lineIdx] == ',') { noteVal = 0; dutyVal = 0; }
+                    else { noteVal = ctoi(line[lineIdx]);}
+
+                    EEPROM.write(romIdx++, noteVal);
+                    EEPROM.write(romIdx++, durVal);
+                    EEPROM.write(romIdx++, dutyVal);
+                }
+            
+                EEPROM.write(romIdx, -1);
+                
+                EEPROM.write(0, 0xAA);
+                EEPROM.write(1, 0x55);
+                EEPROM.write(2, romIdx);  // EEPROM(2) means the length of valid values include (0~2)
+                EEPROM.commit();
+                
+                Serial.println("Saving complete");
+            
+            }
+            // play withdout saving
+            else {
+            //Serial.printf("input line : %s\n", line);
+                Serial.println("Playing...");
+                for (int i = 0; i + 1 < len; i+=2) {
+                    delay(10);
+                    int noteVal, dutyVal = 128, durVal = ctoi(line[i+1]);
+                    
+                    if (line[i] == ',') { noteVal = 0; dutyVal = 0; }
+                    else { noteVal = ctoi(line[i]);}
+
+                    playNote(noteVal, durVal, dutyVal);
+                }
+            }
+
+            
+        } // end of input mode check
     }
 
     WiFiClient client = server.available(); // Listen for incoming clients
@@ -132,6 +225,7 @@ void checkHour(String str) {
 }
 
 void checkMinute(String str) {
+
     static int firstSlashIndex = str.indexOf("/");
     // xxx.xxx.xxx.xxx/set/h**/m**
     
@@ -139,4 +233,25 @@ void checkMinute(String str) {
     int b = str.charAt(firstSlashIndex + 11) - '0';
     
     alarmMinute = 10*a + b;
+}
+
+int ctoi(char c) {
+  return ('0' <= c && c <= '9' ? (int)(c - '0') : ('a' <= c && c <= 'z' ? (int)(c - 'a' + 10) : -1));
+}
+
+void playNote(int note, int dur, int duty) {
+  // not a code for user
+  if (dur == -1) {
+    ledcSetup(ledChannel, 0, resolution);
+    ledcWrite(ledChannel, 0);
+    delay(1);
+    return;
+  }
+  
+  ledcSetup(ledChannel, nFrq[note], resolution);
+  ledcWrite(ledChannel, duty);
+  //Serial.println(duty == 0 ? -1 : note);
+  
+  //Serial.printf("%d %d %d\n", note, dur, duty);
+  delay(nDur[dur]);
 }
