fc75c59fc5a0d4d01b3c521400091841dad7143f
Pumpkinjam <rnjsl1212@naver.com>
2022-05-17 18:32:07 +0900

    Add SMS_Send Sample Code (Python)

---
+++ Twilio(SMS)/Sample_Code.py
@@ -0,0 +1,25 @@
+import json
+from twilio.rest import Client 
+
+# get JSON payload with form {"Message" : "asdfasdf"}
+def lambda_handler(event, context):
+    # TODO implement
+    
+    ms = event['Message']
+    
+    account_sid = 'ACce0456681b1d61bc9de849a8e370253e' 
+    auth_token = '[AuthToken]' 
+    client = Client(account_sid, auth_token) 
+ 
+    message = client.messages.create(
+        messaging_service_sid='MG1082d7cd0834adeba6ef73528805bfa9', 
+        body=ms,      
+        to='+821081389633' 
+        ) 
+ 
+    print(message.sid)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps(ms)
+    }
